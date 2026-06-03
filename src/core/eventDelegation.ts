2f9f4145a55430dbf197ad9b85bb953fd07209a6
Ololade <73138104+zololade@users.noreply.github.com>
2026-06-03 17:32:18 +0100

    feat: add event delegator

---
+++ src/core/eventDelegation.ts
@@ -0,0 +1,35 @@
+import { app } from "../main";
+type SelectHnd = {
+  selector: string;
+  handler: (match: HTMLElement, e: Event) => void;
+};
+
+let eventsMap = new Map<string, SelectHnd[]>([
+  ["click", [{ selector: "string", handler: dummyEvent }]],
+]);
+
+let events = new Set(eventsMap.keys());
+
+events.forEach((val) => {
+  if (!app) return;
+
+  let event = eventsMap.get(val);
+  if (event) {
+    app.addEventListener(val, (e) => {
+      let target = e.target as HTMLElement;
+      for (const { selector, handler } of event) {
+        if (target) {
+          let match = target.closest(selector) as HTMLElement;
+          if (match) {
+            handler(match, e);
+            break;
+          }
+        }
+      }
+    });
+  }
+});
+
+function dummyEvent() {
+  console.log("hello");
+}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import "./styles/app.css";
 
+//declear the app
+let app = document.querySelector("#app");
+
 window.addEventListener("load", () => {
   document.body.style.opacity = "1";
 });
+
+export { app };
