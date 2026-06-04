016c0c030d8ebf85474de49f6ecf2f5a75af49c0
Ololade <73138104+zololade@users.noreply.github.com>
2026-06-04 13:03:23 +0100

    fix: remove console log

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,10 +2,7 @@ function hasKeys(value: unknown, keys: readonly string[]): boolean {
   return (
     typeof value === "object" &&
     value !== null &&
-    keys.every((key) => {
-      console.log(key);
-      return key in value;
-    })
+    keys.every((key) => key in value)
   );
 }
 export { hasKeys };
