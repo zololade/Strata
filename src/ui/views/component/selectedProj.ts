b6de3604c0c568bc5bf28206626f84db8f2ce3a8
Ololade <73138104+zololade@users.noreply.github.com>
2026-06-14 23:01:40 +0100

    feat: add inline edit events

--- src/ui/views/component/selectedProj.ts
+++ src/ui/views/component/selectedProj.ts
@@ -1,50 +1,56 @@
 import type { ProjectInstance, StoredType } from "../../../lib/Types";
 
 //selected project data
-let selectedProj = (project: ProjectInstance, _store: StoredType) => [
-  {
+let selectedProj = (project: ProjectInstance, _store: StoredType) => {
+  return {
     tag: "div",
-    class: "decIcon",
-    ["aria-hidden"]: true,
+    class: "contentContainer",
     content: [
       {
-        tag: "span",
-        class: "material-symbols-outlined",
-        content: "layers",
-      },
-    ],
-  },
-  {
-    tag: "h2",
-    contenteditable: true,
-    ["data-action"]: "update-title prevent-newline",
-    content: project.title,
-    "data-placeholder": "New project",
-  },
-  {
-    tag: "ul",
-    class: "flags",
-    content: ["important", "favorite"].map((val) => {
-      return {
-        tag: "li",
+        tag: "div",
+        class: "decIcon",
+        ["aria-hidden"]: true,
         content: [
           {
-            tag: "button",
-            class: "flagsBtn",
-            content: val,
+            tag: "span",
+            class: "material-symbols-outlined",
+            content: "layers",
           },
         ],
-      };
-    }),
-  },
-  { tag: "hr" },
-  {
-    tag: "p",
-    contenteditable: true,
-    "data-placeholder": "Project overview...",
-    ["data-action"]: "update-overview prevent-newline",
-    content: project.overview,
-  },
-];
+      },
+      {
+        tag: "h2",
+        contenteditable: true,
+        ["data-action"]: "update-title prevent-newline",
+        content: project.title,
+        "data-placeholder": "New project",
+      },
+      {
+        tag: "ul",
+        class: "flags",
+        content: ["important", "favorite"].map((val) => {
+          return {
+            tag: "li",
+            content: [
+              {
+                tag: "button",
+                class: "flagsBtn",
+                content: val,
+              },
+            ],
+          };
+        }),
+      },
+      { tag: "hr" },
+      {
+        tag: "p",
+        contenteditable: true,
+        "data-placeholder": "Project overview...",
+        ["data-action"]: "update-overview prevent-newline",
+        content: project.overview,
+      },
+    ],
+  };
+};
 
 export { selectedProj };
