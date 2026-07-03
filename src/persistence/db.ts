c8f44210781679c79600262943916936ff9d3f8c
Ololade <73138104+zololade@users.noreply.github.com>
2026-07-03 21:18:37 +0100

    refactor: streamline object store existence checks in createDatabase function

--- src/persistence/db.ts
+++ src/persistence/db.ts
@@ -13,21 +13,17 @@ function createDatabase(wrapper: Wrapper) {
 
     request.addEventListener("upgradeneeded", function () {
       const db = request.result;
-      const projectStoreExist = db.objectStoreNames.contains(PROJECT_STORE);
-      const taskStoreExist = db.objectStoreNames.contains(TASK_STORE);
-      const itemStoreExist = db.objectStoreNames.contains(ITEM_STORE);
-      const metaStoreExist = db.objectStoreNames.contains(META_STORE);
 
-      if (!projectStoreExist) {
+      if (!db.objectStoreNames.contains(PROJECT_STORE)) {
         db.createObjectStore(PROJECT_STORE, { keyPath: "id" });
       }
-      if (!taskStoreExist) {
+      if (!db.objectStoreNames.contains(TASK_STORE)) {
         db.createObjectStore(TASK_STORE, { keyPath: "id" });
       }
-      if (!itemStoreExist) {
+      if (!db.objectStoreNames.contains(ITEM_STORE)) {
         db.createObjectStore(ITEM_STORE, { keyPath: "id" });
       }
-      if (!metaStoreExist) {
+      if (!db.objectStoreNames.contains(META_STORE)) {
         db.createObjectStore(META_STORE, { keyPath: "key" });
       }
     });
